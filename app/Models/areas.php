fe47a71df5ee77367a5a3de2a3ef787eed7d3377
Eber Palma <al221911577@gmail.com>
2022-02-25 12:55:12 -0600

    oldvvde este modelo

--- app/Models/areas.php
+++ app/Models/areas.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class areas extends Model
 {
     use HasFactory;
+    protected $table = 'areas';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'nombre'
+    ];
 }
